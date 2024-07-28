1e2712002d1097b6293a4cb37b37d86c2681657a
molliean <molliean29@gmail.com>
2024-07-28 12:15:10 -0700

    added port variable

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ const usersRouter = require('./controllers/users');
 const profilesRouter = require('./controllers/profiles');
 const boardsRouter = require('./controllers/boards');
 
+const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+const allowedOrigins = ['https://main--onboard-buddy.netlify.app'];
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
@@ -26,6 +29,6 @@ app.use('/users', usersRouter);
 app.use('/profiles', profilesRouter);
 app.use('/boards', boardsRouter);
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('The express app is ready!');
 });
